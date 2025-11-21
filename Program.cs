4e90c475f1bd2cb6d5a1a3b22a73af84654ca3b9
gerard <gerard.alonso.7e9@itb.cat>
2025-11-21 19:04:24 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+Cousing System;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Program
+{
+    static void Main()
+    {
+
+    }
+
+}
